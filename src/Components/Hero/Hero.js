78559fb71eea0edcc8d56c2b8ad6f4838382a165
Rohith Challa <rohithsince2002@gmail.com>
2022-04-23 21:57:23 +0530

    defines styles for hero .

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -4,6 +4,10 @@ import styles from './Hero.module.css'
 function Hero() {
   return (
     <div className={styles.hero_container}>
+      <div className={styles.hero_content_container}>
+      <h1 className={styles.hero_content_heading}>Computer Engineering</h1>
+      <h3 className={styles.hero_content_para}>142,765 Computer Engineers follow this</h3>
+      </div>
     </div>
   )
 }
