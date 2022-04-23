564cc74442d1aec2d5e191a87c07bcca4e500fe3
Rohith Challa <rohithsince2002@gmail.com>
2022-04-23 21:40:46 +0530

    define styles for hero background.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import styles from './Hero.module.css'
-import hero from './hero1x.svg'
 
 function Hero() {
   return (
     <div className={styles.hero_container}>
-      <img className={styles.hero_img} src={hero} alt='hero img'/>
     </div>
   )
 }
