b3984acde78316b1128befe88010651cd37e1d11
Rohith Challa <rohithsince2002@gmail.com>
2022-04-23 21:03:22 +0530

    check mobile version of hero.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,11 @@
 import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/Hero'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from './Hero.module.css'
+import hero from './hero1x.svg'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className={styles.hero_container}>
+      <img className={styles.hero_img} src={hero} alt='hero img'/>
+    </div>
   )
 }
 
